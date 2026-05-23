984cd47c6f703a0041990d1cee9d7caae095324e
Zer0M4n <l22211575@tectijuana.edu.mx>
2026-05-22 20:41:20 -0700

    add more arithmetics operators

--- src/math/point.rs
+++ src/math/point.rs
@@ -1,11 +1,10 @@
-use crate::math::vector::Vector;
 use crate::math::utils::comparing_floating_number;
+use crate::math::vector::Vector;
 use std::cmp::PartialEq;
-use std::ops::Add;
-use std::ops::Sub;
+use std::ops::{Add, Mul, Neg, Sub,Div};
 
 //The logic point , w is 1.0 for math facilities
-#[derive(Debug)]
+#[derive(Debug,Clone, Copy)]
 pub struct Point {
     pub x: f64,
     pub y: f64,
@@ -23,30 +22,34 @@ impl Point {
             && comparing_floating_number(self.y, v2.y)
             && comparing_floating_number(self.z, v2.z)
     }
-    fn add_vector(&self, other:&Vector) -> Self {
-        Point::new(
-            self.x + other.x, 
-            self.y + other.y, 
-            self.z + other.z
-        )
+    fn add_vector(&self, other: &Vector) -> Self {
+        Point::new(self.x + other.x, self.y + other.y, self.z + other.z)
     }
-    fn subtrac_point(&self, other:&Self) -> Vector {
-        Vector::new(
-            self.x - other.x , 
-            self.y - other.y, 
-            self.z - other.z
-        )
+    fn subtrac_point(&self, other: &Self) -> Vector {
+        Vector::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
-    fn vector_subtrac_point(&self , other:Vector) -> Point {
-        Point::new(
-            self.x - other.x, 
-            self.y - other.y, 
-            self.z - other.z
-        )
+    fn vector_subtrac_point(&self, other: Vector) -> Point {
+        Point::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
+    fn multi_scalar(&self, scalar: f64) -> Self {
 
+        Point::new(
+            self.x * scalar, 
+            self.y * scalar, 
+            self.z * scalar 
+        ) 
+        
+    }
+    fn div_scalar(&self, scalar: f64) -> Self {
+        Point::new(
+            self.x / scalar, 
+            self.y / scalar, 
+            self.z / scalar 
+        )
+    }
 }
 
+//Arithmetic Operators Overload
 impl Add<Vector> for Point {
     type Output = Point;
 
@@ -54,19 +57,12 @@ impl Add<Vector> for Point {
         self.add_vector(&rhs)
     }
 }
-impl PartialEq for Point {
-    fn eq(&self, other: &Point) -> bool {
-        self.equal_point(other)
-    }
-}
-
 impl Sub for Point {
     type Output = Vector;
-    
+
     fn sub(self, rhs: Self) -> Self::Output {
         self.subtrac_point(&rhs)
     }
-    
 }
 impl Sub<Vector> for Point {
     type Output = Point;
@@ -74,7 +70,35 @@ impl Sub<Vector> for Point {
     fn sub(self, rhs: Vector) -> Self::Output {
         self.vector_subtrac_point(rhs)
     }
+}
+impl Mul<f64> for Point {
+    type Output = Point;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        self.multi_scalar(rhs)
+    }
     
+}
+impl Div<f64> for Point {
+    type Output = Point;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self.div_scalar(rhs)
+    }
+    
+}
+impl Neg for Point {
+    type Output = Point;
+    fn neg(self) -> Self::Output {
+        Point::new(-self.x, -self.y, -self.z)
+    }
+}
+
+//Boolean Operators Overload
+impl PartialEq for Point {
+    fn eq(&self, other: &Point) -> bool {
+        self.equal_point(other)
+    }
 }
 #[cfg(test)]
 mod tests {
@@ -100,37 +124,51 @@ mod tests {
     #[test]
     fn point_add_vector() {
         let p = Point::new(1.0, 2.0, 3.0);
-        let p2 = Vector::new(2.0, 3.0, 4.0) ;
-        
+        let p2 = Vector::new(2.0, 3.0, 4.0);
+
         assert_eq!(
-            p+p2, 
-            Point{
-                x:3.0,
-                y:5.0,
-                z:7.0,
-                w:1.0
-        })
+            p + p2,
+            Point {
+                x: 3.0,
+                y: 5.0,
+                z: 7.0,
+                w: 1.0
+            }
+        )
     }
     #[test]
     fn point_subtrac_point() {
         let p = Point::new(3.0, 2.0, 1.0);
         let p2 = Point::new(5.0, 6.0, 7.0);
 
-        assert_eq!(
-            p - p2,
-            Vector::new(-2.0, -4.0, -6.0)
-        )
-        
+        assert_eq!(p - p2, Vector::new(-2.0, -4.0, -6.0))
     }
 
     #[test]
     fn p_sub_v() {
-        let p = Point::new(3.0, 2.0,1.0);
+        let p = Point::new(3.0, 2.0, 1.0);
         let v = Vector::new(5.0, 6.0, 7.0);
 
-        assert_eq!(
-            p - v,
-            Point::new(-2.0, -4.0, -6.0)
-        )
+        assert_eq!(p - v, Point::new(-2.0, -4.0, -6.0))
+    }
+    #[test]
+    fn point_negation() {
+        let p = Point::new(3.0, 2.0, 1.0);
+
+        assert_eq!(-p, Point::new(-3.0, -2.0, -1.0))
+    }
+    #[test]
+    fn scalar_multi_point() {
+        let p = Point::new(1.0, -2.0, 3.0);
+
+        assert_eq!(p * 3.5, Point::new(3.5, -7.0, 10.5));
+        assert_eq!(p * 0.5, Point::new(0.5, -1.0, 1.5));
+        
+    }
+    #[test]
+    fn scalar_div_point() {
+        let p = Point::new(1.0, -2.0, 3.0);
+
+        assert_eq!(p / 2.0,Point::new(0.5, -1.0, 1.5));
     }
 }
--- src/math/vector.rs
+++ src/math/vector.rs
@@ -1,9 +1,9 @@
 use crate::math::point::Point;
 use crate::math::utils::comparing_floating_number;
 use std::cmp::PartialEq;
-use std::ops::{Add, Sub, Neg};
+use std::ops::{Add, Neg, Sub,Mul,Div};
 //The logic vector , w is 0.0 for math facilities
-#[derive(Debug)]
+#[derive(Debug,Clone, Copy)]
 pub struct Vector {
     pub x: f64,
     pub y: f64,
@@ -24,21 +24,54 @@ impl Vector {
     fn add_vector(&self, other: &Self) -> Vector {
         Vector::new(self.x + other.x, self.y + other.y, self.z + other.z)
     }
-    fn add_point(&self, p: Point) -> Point{
-        Point::new(
-            self.x +p.x,
-            self.y +p.y,
-            self.z +p.z
-        )
+    fn add_point(&self, p: Point) -> Point {
+        Point::new(self.x + p.x, self.y + p.y, self.z + p.z)
     }
     fn subtrac_vector(&self, other: &Self) -> Self {
-        Vector::new(
-            self.x - other.x, 
-            self.y - other.y, 
-            self.z - other.z
-        )
-
+        Vector::new(self.x - other.x, self.y - other.y, self.z - other.z)
     }
+    fn multi_scalar(&self , scalar: f64) -> Self {
+        Vector::new(
+            self.x * scalar, 
+            self.y * scalar, 
+            self.z * scalar
+        )        
+    }
+    fn div_scalar(&self, scalar: f64) -> Self {
+        Vector::new(
+            self.x / scalar, 
+            self.y / scalar, 
+            self.z / scalar 
+        )
+    }
+    fn lenght(&self) -> f64{
+        (
+            self.x.powi(2) +
+            self.y.powi(2) +
+            self.z.powi(2) +
+            self.w.powi(2) 
+        ).sqrt()
+    }
+    fn normalization(&self) -> Self {
+        Vector::new(
+            self.x / self.lenght(), 
+            self.y / self.lenght(), 
+            self.z / self.lenght()
+        )
+    }
+    fn dot_product(&self, other: Self) -> f64 {
+        (self.x * other.x) +
+        (self.y * other.y) +
+        (self.z * other.z)
+    }
+    fn cross_product(&self, other: Self) -> Self {
+        Vector::new(
+            (self.y * other.z) - (self.z * other.y), 
+            (self.z * other.x) - (self.x * other.z), 
+            (self.x * other.y) - (self.y * other.x)
+        )
+    }
+    
 }
 
 //Arithmetic Operators Overload
@@ -54,18 +87,30 @@ impl Sub for Vector {
     fn sub(self, rhs: Self) -> Self::Output {
         self.subtrac_vector(&rhs)
     }
-    
 }
 
 impl Neg for Vector {
     type Output = Vector;
 
     fn neg(self) -> Self::Output {
-        Vector::new(
-            -self.x, 
-            -self.y, 
-            -self.z
-        )
+        Vector::new(-self.x, -self.y, -self.z)
+    }
+}
+
+impl Mul<f64> for Vector {
+
+    type Output = Vector;
+
+    fn mul(self, rhs: f64) -> Self::Output {
+        self.multi_scalar(rhs)
+    }
+    
+}
+impl Div<f64> for Vector {
+    type Output = Vector;
+
+    fn div(self, rhs: f64) -> Self::Output {
+        self.div_scalar(rhs)
     }
     
 }
@@ -76,8 +121,6 @@ impl PartialEq for Vector {
         self.equal_vector(other)
     }
 }
-
-
 
 #[cfg(test)]
 mod tests {
@@ -123,26 +166,62 @@ mod tests {
         let p = Point::new(3.0, -2.0, 5.0);
         let v = Vector::new(-2.0, 3.0, 1.0);
 
-        assert_eq!(
-            p + v ,Point::new(1.0, 1.0, 6.0)
-        )    
+        assert_eq!(p + v, Point::new(1.0, 1.0, 6.0))
     }
     #[test]
     fn vector_sub_vector() {
         let v = Vector::new(3.0, 2.0, 1.0);
         let v2 = Vector::new(5.0, 6.0, 7.0);
-        assert_eq!(
-            v - v2,
-            Vector::new(-2.0, -4.0, -6.0)
-        )
+        assert_eq!(v - v2, Vector::new(-2.0, -4.0, -6.0))
     }
+    #[test]
+    fn scalar_multi_vector() {
+        let v = Vector::new(1.0, -2.0, 3.0);
+
+        assert_eq!(v * 3.5, Vector::new(3.5, -7.0, 10.5));
+        assert_eq!(v * 0.5, Vector::new(0.5, -1.0, 1.5));
+        
+    }
+    #[test]
+    fn scalar_div_point() {
+        let p = Vector::new(1.0, -2.0, 3.0);
+
+        assert_eq!(p / 2.0,Vector::new(0.5, -1.0, 1.5));
+    }
+
     #[test]
     fn neg_vector() {
         let v = Vector::new(3.0, 2.0, 1.0);
 
         assert_eq!(-v, Vector::new(-3.0, -2.0, -1.0))
-        
     }
+    #[test]
+    fn magnitude() {
+        let v = Vector::new(0.0, 0.0, 1.0);
+        let v2 = Vector::new(1.0, 2.0, 3.0);
+        let h:f64 = 14.0;
+        assert_eq!(v.lenght(), 1.0);
+        assert_eq!(v2.lenght(), h.sqrt());
+    }
+    #[test]
+    fn normalizing_vector() {
+        let v = Vector::new(1.0, 2.0, 3.0);
 
+        assert_eq!(v.normalization(),Vector::new(0.26726, 0.53452, 0.80178))
+    }
+    #[test]
+    fn vector_dot_product() {
+        let v = Vector::new(1.0, 2.0, 3.0);
+        let v1 = Vector::new(2.0, 3.0, 4.0);
 
+        assert_eq!(v.dot_product(v1),20.0);
+    }
+     #[test]
+    fn vector_cross_product() {
+        let v = Vector::new(1.0, 2.0, 3.0);
+        let v1 = Vector::new(2.0, 3.0, 4.0);
+
+        assert_eq!(v.cross_product(v1) ,Vector::new(-1.0, 2.0, -1.0));
+        assert_eq!(v1.cross_product(v) ,Vector::new(1.0, -2.0, 1.0));
+    }
 }
