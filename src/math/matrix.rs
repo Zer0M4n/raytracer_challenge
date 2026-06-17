0452a8ff7724e5ce5817a1aaa82d0610bfeaf16b
Zer0M4n <l22211575@tectijuana.edu.mx>
2026-06-16 19:43:46 -0700

    add default matrix and  add new test

--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,3 +1,4 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub struct Matrix4 {
     data: [[f64; 4]; 4],
 }
@@ -6,13 +7,25 @@ impl Matrix4 {
     pub fn new(data: [[f64; 4]; 4]) -> Self {
         Matrix4 { data }
     }
-
     pub fn get(&self, row: usize, col: usize) -> f64 {
         self.data[row][col]
     }
 
     pub fn set(&mut self, row: usize, col: usize, value: f64) {
         self.data[row][col] = value;
+    }
+}
+
+impl Default for Matrix4 {
+    fn default() -> Self {
+        Self {
+            data: [
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+                [0.0, 0.0, 0.0, 0.0],
+            ],
+        }
     }
 }
 
@@ -48,5 +61,40 @@ mod test {
         matrix.set(0, 0, 0.0);
 
         assert_eq!(matrix.get(0, 0), 0.0);
+    }
+    #[test]
+    fn equal_matrix() {
+        let data = [
+            [1.0, 2.0, 3.0, 2.0],
+            [4.0, 5.0, 6.0, 5.0],
+            [7.0, 9.0, 5.0, 7.0],
+            [1.0, 2.0, 3.0, 3.0],
+        ];
+
+        let matrix1 = Matrix4::new(data);
+        let matrix2 = Matrix4::new(data);
+
+        assert_eq!(matrix1, matrix2);
+    }
+    #[test]
+    fn equality_with_different_matrices() {
+        let data = [
+            [1.0, 2.0, 3.0, 4.0],
+            [5.0, 6.0, 7.0, 8.0],
+            [9.0, 8.0, 7.0, 6.0],
+            [5.0, 4.0, 3.0, 2.0],
+        ];
+
+        let data2 = [
+            [2.0, 3.0, 4.0, 5.0],
+            [6.0, 7.0, 8.0, 9.0],
+            [8.0, 7.0, 6.0, 5.0],
+            [4.0, 3.0, 2.0, 1.0],
+        ];
+
+        let matrix1 = Matrix4::new(data);
+        let matrix2 = Matrix4::new(data2);
+
+        assert!(matrix2 != matrix1);
     }
 }
