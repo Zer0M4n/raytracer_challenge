ac4c9db57187be64ee7a5d881dae9279c56e00c1
Zer0M4n <l22211575@tectijuana.edu.mx>
2026-06-18 21:11:05 -0700

    implement matrix multiplication and update indexing in get/set methods

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,17 @@ use crate::math::point::Point;
 use crate::math::vector::Vector;
 use std::fmt::Display;
 
-fn main() {}
+fn main() {
+          let v1 = vec![1.0 , 2.0 , 3.0 , 4.0 ,
+                                2.0 , 4.0 , 4.0 , 2.0 ,
+                                8.0 , 6.0 , 4.0 , 1.0 ,
+                                0.0 , 0.0 , 0.0 , 1.0 ];
+        let tuple = vec![1.0,2.0,3.0,1.0];
+        let r = vec![18.0,24.0,33.0,1.0];
+        let re = Matrix::from_vec(1, 4, r).unwrap();
+        let m = Matrix::from_vec(4, 4, v1).unwrap();
+        let matrix_tuple = Matrix::from_vec(4, 1, tuple).unwrap();
+        let boss =(m * matrix_tuple).unwrap();
+
+    println!("{:?}", boss);
+}
--- src/math/matrix.rs
+++ src/math/matrix.rs
@@ -1,4 +1,5 @@
-use std::ops::ControlFlow::Break;
+use std::cmp::PartialEq;
+use std::ops::Mul;
 
 #[derive(Debug, Clone, PartialEq)]
 
@@ -28,40 +29,53 @@ impl Matrix {
         })
     }
     pub fn get(&self, rows: usize, cols: usize) -> f64 {
-        let index = rows * cols + cols;
+        let index = rows * self.cols + cols;
         self.data[index]
     }
     pub fn set(&mut self, rows: usize, cols: usize, value: f64) {
-        let index = rows * cols + cols;
+        let index = rows * self.cols + cols;
         self.data[index] = value;
     }
 
-    fn multi(&mut self, value: Matrix) -> Result<Self, String> {
-        if self.cols != value.rows {
-            return Err(format!(
-                "The number of columns of the first matrix must equal the number of rows of the second matrix."
-            ));
+    fn multi(&self, rhs: Matrix) -> Result<Matrix, String> {
+        if self.cols != rhs.rows {
+            return Err(
+            "The number of columns of the first matrix must equal the number of rows of the second matrix."
+                .to_string(),
+        );
         }
 
-        let mut m = Matrix::new(self.rows, value.cols);
+        let mut result = Matrix::new(self.rows, rhs.cols);
+
         for r in 0..self.rows {
-            for c in 0..value.cols {
+            for c in 0..rhs.cols {
                 let mut sum = 0.0;
+
                 for k in 0..self.cols {
-                    let data = self.get(r, c) * value.get(r, c);
-                    sum += self.get(r, k) * value.get(k, c);
+                    sum += self.get(r, k) * rhs.get(k, c);
                 }
-                m.set(r, c, sum);
+
+                result.set(r, c, sum);
             }
         }
 
-        Ok(m)
+        Ok(result)
+    }
+}
+
+impl Mul for Matrix {
+    type Output = Result<Matrix, String>;
+
+    fn mul(self, rhs: Matrix) -> Self::Output {
+        self.multi(rhs)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use core::prelude::v1;
+
+use super::*;
 
     #[test]
     fn set_values() {
@@ -70,7 +84,7 @@ mod tests {
         m.set(1, 1, 1.0);
 
         assert_eq!(m.data[0], 0.0);
-        assert_eq!(m.data[1 * 1 + 1], 1.0);
+        assert_eq!(m.data[1 * 4 + 1], 1.0);
     }
     #[test]
     fn get_values() {
@@ -94,5 +108,37 @@ mod tests {
         assert!(m == m2);
         m2.data[0] = 1.0;
         assert!(m != m2);
+    }
+    #[test]
+    fn matrix_multiply() {
+        let v1 = vec![
+            1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0,
+        ];
+        let v2 = vec![
+            -2.0, 1.0, 2.0, 3.0, 3.0, 2.0, 1.0, -1.0, 4.0, 3.0, 6.0, 5.0, 1.0, 2.0, 7.0, 8.0,
+        ];
+        let v3 = vec![
+            20.0, 22.0, 50.0, 48.0, 44.0, 54.0, 114.0, 108.0, 40.0, 58.0, 110.0, 102.0, 16.0, 26.0,
+            46.0, 42.0,
+        ];
+        let m1 = Matrix::from_vec(4, 4, v1).unwrap();
+        let m2 = Matrix::from_vec(4, 4, v2).unwrap();
+        let m3 = Matrix::from_vec(4, 4, v3).unwrap();
+
+        let result = (m1 * m2).unwrap();
+        assert_eq!(result, m3);
+    }
+    #[test]
+    fn multiplied_by_a_tuple() {
+        let v1 = vec![1.0 , 2.0 , 3.0 , 4.0 ,
+                                2.0 , 4.0 , 4.0 , 2.0 ,
+                                8.0 , 6.0 , 4.0 , 1.0 ,
+                                0.0 , 0.0 , 0.0 , 1.0 ];
+        let tuple = vec![1.0,2.0,3.0,1.0];
+        let r = vec![18.0,24.0,33.0,1.0];
+        let re = Matrix::from_vec(4, 1, r).unwrap();
+        let m = Matrix::from_vec(4, 4, v1).unwrap();
+        let matrix_tuple = Matrix::from_vec(4, 1, tuple).unwrap();
+        assert_eq!((m * matrix_tuple).unwrap() , re);
     }
 }
