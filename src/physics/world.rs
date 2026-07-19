0e651719316ee65521d0e3ddc47d7485cc3be93d
Zer0M4n <l22211575@tectijuana.edu.mx>
2026-07-19 13:45:55 -0700

    feat: add intersection functionality to World struct and update tests

--- src/physics/world.rs
+++ src/physics/world.rs
@@ -1,7 +1,7 @@
 use crate::{
     color::Color,
     math::{matrix::Matrix, point::Point},
-    physics::{material::*, sphere::Sphere},
+    physics::{intersect::Intersection, material::*, ray::Ray, sphere::Sphere},
 };
 
 pub struct World {
@@ -32,14 +32,25 @@ impl World {
     pub fn add_object(&mut self, object: Sphere) {
         self.objects.push(object);
     }
+    pub fn intersect_world(&self, r: Ray) -> Vec<Intersection<'_>> {
+        let mut xs = Vec::new();
+
+        for object in &self.objects {
+            xs.extend(object.intersect(r));
+        }
+
+        xs.sort_by(|a, b| a.t.partial_cmp(&b.t).unwrap());
+
+        xs
+    }
 }
 
 #[cfg(test)]
 
 mod tests {
     use crate::{
-        math::matrix::Matrix,
-        physics::{material::Material, sphere},
+        math::{matrix::Matrix, vector::Vector},
+        physics::{material::Material, ray::Ray, sphere},
     };
 
     use super::*;
@@ -63,5 +74,19 @@ mod tests {
         assert_eq!(w.light, light);
         assert!(w.objects.contains(&s1));
         assert!(w.objects.contains(&s2));
+    }
+    #[test]
+    fn intersect_a_world_with_a_ray() {
+        let w = World::default();
+        let r = Ray::new(Point::new(0.0, 0.0, -5.0), Vector::new(0.0, 0.0, 1.0));
+
+        let xs = w.intersect_world(r);
+
+        assert_eq!(xs.len(), 4);
+
+        assert_eq!(xs[0].t, 4.0);
+        assert_eq!(xs[1].t, 4.5);
+        assert_eq!(xs[2].t, 5.5);
+        assert_eq!(xs[3].t, 6.0);
     }
 }
